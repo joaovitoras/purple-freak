fc5441d3e1991508d86f8e7f4e64e19049853188
João Vitor Alves <joao.vitor@redealumni.com>
2020-08-26 16:48:12 -0300

    adds manifest initial

--- client/src/components/manifest.vue
+++ client/src/components/manifest.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <div
-      v-for="(bullet, index) in bullets"
-      :key="index"
-    >
-      {{ index + 1 }} - {{ bullet }}
+  <div class="manifest-section">
+    <div class="manifest-section__video">
+      Video
+    </div>
+    <div class="manifest-section__items">
+      <template
+        v-for="(bullet, index) in bullets"
+      >
+        <h2
+          :key="index"
+          class="manifest-section__item"
+        >
+          <z-text
+            tag="span"
+            weight="bold"
+            class="manifest-section__number"
+          >
+            {{ index + 1 }}
+          </z-text>
+          <z-text>{{ bullet }}</z-text>
+        </h2>
+      </template>
     </div>
   </div>
 </template>
@@ -19,3 +35,28 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import '@quero/zilla-core/src/utils/_index.scss';
+
+.manifest-section {
+  display: flex;
+}
+
+.manifest-section__video, .manifest-section__items {
+  flex-basis: 0;
+  flex-grow: 1;
+}
+
+.manifest-section__item {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: var(--space-large);
+}
+
+.manifest-section__number.manifest-section__number {
+  font-size: rem(62);
+  line-height: 1;
+  margin-right: var(--space-medium);
+}
+</style>
--- client/src/plugins/zilla.js
+++ client/src/plugins/zilla.js
@@ -13,8 +13,8 @@ Vue.component('ZCarousel', () => import('@quero/zilla-vue/src/molecules/z-carous
 // Vue.component('ZLink', () => import('@quero/zilla-vue/src/atoms/z-link/z-link.vue'));
 // Vue.component('ZLoading', () => import('@quero/zilla-vue/src/atoms/z-loading/z-loading.vue'));
 // Vue.component('ZPopover', () => import('@quero/zilla-vue/src/atoms/z-popover/z-popover.vue'));
-// Vue.component('ZText', () => import('@quero/zilla-vue/src/atoms/z-text/z-text.vue'));
-// Vue.component('ZTitle', () => import('@quero/zilla-vue/src/atoms/z-title/z-title.vue'));
+Vue.component('ZText', () => import('@quero/zilla-vue/src/atoms/z-text/z-text.vue'));
+Vue.component('ZTitle', () => import('@quero/zilla-vue/src/atoms/z-title/z-title.vue'));
 
 // Vue.component('ZAccordion', () => import('@quero/zilla-vue/src/molecules/z-accordion/z-accordion.vue'));
 // Vue.component('ZBreadcrumb', () => import('@quero/zilla-vue/src/molecules/z-breadcrumb/z-breadcrumb.vue'));
