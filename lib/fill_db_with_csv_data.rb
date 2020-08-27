7e2ae6cc31d29a8b13dfaad3ac8df84b35a6354d
caiam <caiam.rct@gmail.com>
2020-08-26 21:37:24 -0300

    updates csv and script

--- lib/fill_db_with_csv_data.rb
+++ lib/fill_db_with_csv_data.rb
@@ -9,21 +9,24 @@ def run!
   @csv = CSV.read(@file_path, csv_options)
   @errors = []
   @csv.each do |row|
+    next unless row[:full_name].present?
+
     lead = convert_row_to_lead(row)
     process_lead_or_add_errors(lead)
   end
   puts @errors.count
   puts @errors.uniq
 end
+# csv headers: email,data_ultima_conversao,full_name,profissao
 
 def convert_row_to_lead(row)
   init_params = {
-    full_name: row[:nome_completo],
-    signed_at: row[:data_da_primeira_converso],
+    full_name: row[:full_name],
+    signed_at: row[:data_ultima_conversao],
     email: row[:email],
-    profession: row[:profisso],
-    created_at: row[:data_da_primeira_converso],
-    updated_at: row[:data_da_primeira_converso]
+    profession: row[:profissao],
+    created_at: row[:data_ultima_conversao],
+    updated_at: row[:data_ultima_conversao]
   }
   Lead.new(init_params)
 end
